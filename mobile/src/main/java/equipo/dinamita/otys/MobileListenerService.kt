b4a03987e909ffd85b540fb66d326ae20f95d7cd
Emmmanuel <juegosps585@gmail.com>
2025-07-14 17:14:19 -0600

    Agrego archivos nuevos y modifico sensor manager para mejorar el manejo del ritmo cardíaco

---
+++ mobile/src/main/java/equipo/dinamita/otys/MobileListenerService.kt
@@ -0,0 +1,24 @@
+package equipo.dinamita.otys
+
+import android.content.Intent
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.WearableListenerService
+
+class MobileListenerService : WearableListenerService() {
+
+    override fun onMessageReceived(messageEvent: MessageEvent) {
+        if (messageEvent.path == "/sensor_data_path") {
+            val message = String(messageEvent.data)
+            Log.d("MobileListener", "Mensaje recibido: $message")
+
+            val intent = Intent("SENSOR_DATA_UPDATE")
+            intent.putExtra("sensorData", message)
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+        }git add .gitignore
+        git add wear/src/main/java/equipo/dinamita/otys/presentation/sensors/HeartRateSensorManager.kt
+        git add mobile/src/main/java/equipo/dinamita/otys/MobileListenerService.kt
+
+    }
+}
